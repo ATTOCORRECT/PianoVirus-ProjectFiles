b4e69dd63b076eb24501ba13d02a83f091d6a49f
eloravw <67245265+eloravw@users.noreply.github.com>
2025-02-18 11:13:27 -0500

    Woooahhhh functional

--- just_space_things_godot/Scripts/new_message_node.gd
+++ just_space_things_godot/Scripts/new_message_node.gd
@@ -11,18 +11,22 @@ var sender_rtl: RichTextLabel
 var message_rtl: RichTextLabel
 var sender_pfp: Sprite2D
 
-func _init(new_message_data) -> void:
-	message_data = new_message_data
-	sender = message_data.sender.name
-	message = message_data.message
+func _init() -> void:
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sender_rtl = $message/sender_panel/sender
-	message_rtl = $message/message_body
-	sender_pfp = $message/sender_panel/PanelContainer/Sprite2D
+	pass
+	
+func construct(new_message_data) -> void:
+	message_data = new_message_data
+	sender = message_data.sender.name
+	message = message_data.message
+	
+	sender_rtl = $message_panel/message/sender_panel/sender
+	message_rtl = $message_panel/message/message_body
+	sender_pfp = $message_panel/message/sender_panel/PanelContainer/Sprite2D
 	
 	sender_rtl.set_text(sender)
 	sender_pfp.texture = message_data.sender.pfp
 	message_rtl.set_text(message)
-	 
