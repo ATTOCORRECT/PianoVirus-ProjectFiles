d635468574bab87945dffd1d704d721e50a6530f
T-Cog <tcoghlan3@live.ca>
2025-03-12 22:47:58 -0400

    wip again

--- just_space_things_godot/Scripts/Minigame Scripts/input_minigame_test.gd
+++ just_space_things_godot/Scripts/Minigame Scripts/input_minigame_test.gd
@@ -3,13 +3,15 @@ extends Control
 var correct_code
 var user_code
 
+var code_label
+var userCode_label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	correct_code[0] = randf_range(0, 9)
 	correct_code[1] = randf_range(0, 9)
 	correct_code[2] = randf_range(0, 9)
 	correct_code[3] = randf_range(0, 9)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
