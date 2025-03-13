b423455252b2b1b397db9f14da085102c8c6cdee
ATTOCORRECT <97545662+ATTOCORRECT@users.noreply.github.com>
2025-03-12 22:29:30 -0400

    updated console assets

--- just_space_things_godot/Scripts/button.gd
+++ just_space_things_godot/Scripts/button.gd
@@ -1,19 +1,24 @@
 extends Node3D
 
+@export var mesh : MeshInstance3D
+@onready var material = mesh.get_surface_override_material(0)
 @export var color : Color
 
+func _ready() -> void:
+	material.albedo_color = color
+
 func disable_button():
-	$CSGCylinder3D.material.albedo_color = Color(color.r * 0.5, color.g * 0.5, color.b * 0.5)
+	material.albedo_color = Color(color.r * 0.5, color.g * 0.5, color.b * 0.5)
 	process_mode = ProcessMode.PROCESS_MODE_DISABLED
 
 func set_color(in_color: Color):
 	color = in_color
 	if process_mode == ProcessMode.PROCESS_MODE_DISABLED:
-		$CSGCylinder3D.material.albedo_color = Color(color.r * 0.5, color.g * 0.5, color.b * 0.5)
+		material.albedo_color = Color(color.r * 0.5, color.g * 0.5, color.b * 0.5)
 	else:
-		$CSGCylinder3D.material.albedo_color = color
+		material.albedo_color = color
 
 func enable_button():
-	$CSGCylinder3D.material.albedo_color = color
+	material.albedo_color = color
 	process_mode = ProcessMode.PROCESS_MODE_INHERIT
 	pass
--- just_space_things_godot/Scripts/warp_button.gd
+++ just_space_things_godot/Scripts/warp_button.gd
@@ -1,15 +1,21 @@
 extends Node3D
 
+@export var mesh : MeshInstance3D
+@onready var material = mesh.get_surface_override_material(0)
+@export var color : Color
 
+func _ready() -> void:
+	material.albedo_color = color
 
 func disable_warp_button():
 	#visible = false
-	$CSGCylinder3D.material.albedo_color = Color.DARK_RED
+	material.albedo_color = Color(color.r * 0.5, color.g * 0.5, color.b * 0.5)
 	process_mode = ProcessMode.PROCESS_MODE_DISABLED
 	pass
 
 func enable_warp_button():
 	#visible = true
-	$CSGCylinder3D.material.albedo_color = Color.RED
+	
+	material.albedo_color = color
 	process_mode = ProcessMode.PROCESS_MODE_INHERIT
 	pass
