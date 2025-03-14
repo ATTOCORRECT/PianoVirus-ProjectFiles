38bdd09cb1b2a37873c7cf8422c3fcf98a7bc306
T-Cog <tcoghlan3@live.ca>
2025-03-14 17:01:50 -0400

    Input panel wip

--- just_space_things_godot/Scripts/Minigame Scripts/input_minigame_test.gd
+++ just_space_things_godot/Scripts/Minigame Scripts/input_minigame_test.gd
@@ -100,3 +100,63 @@ func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("Select"):
 		print("User Code: " + str(user_code))
+
+
+func _Button_1_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 1
+		_update_user_code()
+		_check_user_code()
+
+
+func _Button_2_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 2
+		_update_user_code()
+		_check_user_code()
+
+
+func _Button_3_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 3
+		_update_user_code()
+		_check_user_code()
+
+
+func _Button_4_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 4
+		_update_user_code()
+		_check_user_code()
+
+
+func _Button_5_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 5
+		_update_user_code()
+		_check_user_code()
+
+
+func _Button_6_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 6
+		_update_user_code()
+		_check_user_code()
+
+func _Button_7_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 7
+		_update_user_code()
+		_check_user_code()
+
+func _Button_8_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 8
+		_update_user_code()
+		_check_user_code()
+
+func _Button_9_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("Select"):
+		user_code[code_position] = 9
+		_update_user_code()
+		_check_user_code()
