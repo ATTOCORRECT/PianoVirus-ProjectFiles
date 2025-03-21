ce3c76b32986200705d6c14b7151185e4a653987
Jespobuiz09 <151651783+Jespobuiz09@users.noreply.github.com>
2025-03-21 14:39:29 -0400

    added trend select sound

--- just_space_things_godot/Scripts/audio_manager.gd
+++ just_space_things_godot/Scripts/audio_manager.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var button_speaker = $Button
 @onready var minigamesuccess_speaker = $MinigameSuccess
 @onready var minigamefail_speaker = $MinigameFail
+@onready var trend_select_speaker = $TrendSelect
 
 func _enter_tree() -> void:
 	Singleton.audio_manager = %AudioManager
@@ -24,6 +25,9 @@ func play_minigame_success_sound():
 
 func play_minigame_fail_sound():
 	minigamefail_speaker.play()
+
+func play_trend_select_sound():
+	trend_select_speaker.play()
 
 func play_new_sfx(sfx):
 	generic_speaker.stream = sfx
