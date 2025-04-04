564187ab18f3b9aeddbb7d3e8381086e128cefff
eloravw <67245265+eloravw@users.noreply.github.com>
2025-04-04 15:00:41 -0400

    Added onboarding and message sprites

--- just_space_things_godot/Scripts/Direct Messages/dm_panel.gd
+++ just_space_things_godot/Scripts/Direct Messages/dm_panel.gd
@@ -2,11 +2,13 @@ extends Node3D
 
 @onready var messageScreen = $"Interactive Screen/Viewport/direct_messages"
 var dm_screen = preload ("res://direct_messages.tscn")
+@onready var timer = $dm_panel/Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
+func _on_timer_timeout() -> void:
 		messageScreen.new_message()
+		#timer.start(-1)
+	
--- just_space_things_godot/Scripts/direct_messages.gd
+++ just_space_things_godot/Scripts/direct_messages.gd
@@ -22,7 +22,6 @@ func new_message():
 	else:
 		pass
 
-
 func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event.is_action_pressed("Select"):
 		new_message()
